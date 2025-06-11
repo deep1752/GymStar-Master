1af61d25c771de07cafb693c1e254e5b29bf1937
deep1752 <deepakkumawat1752@gmail.com>
2025-06-10 22:44:07 -0400

    update successPay

---
+++ app/(frontend)/testpay/page.js
@@ -0,0 +1,3 @@
+export default function TestPay() {
+  return <h1>This is TestPay page</h1>;
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: false,
-    output: 'standalone',
 };
 
 export default nextConfig;
